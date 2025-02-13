7d59e4070c5695377ac2168eb0f20216bf623373
Herbert Gabriel <alencargabriel1010@gmail.com>
2025-02-13 17:24:32 -0300

    Update microfone.c

--- microfone.c
+++ microfone.c
@@ -7,12 +7,11 @@
 #define MIC_CHANNEL 2
 #define MIC_PIN (26 + MIC_CHANNEL)
 
-#define MIC_THRESHOLD 0.6f  
-#define MIN_PEAKS 3 
+#define MIC_THRESHOLD 0.4f  
+#define MIN_PEAKS 2
 #define ADC_ADJUST(x) (x * 3.3f / (1 << 12u) - 1.65f)
 #define ADC_CLOCK_DIV 96.f
-#define SAMPLE_COUNT 50  
-#define SMOOTHING_FACTOR 0.2f 
+
 
 void mic_init() {
     adc_gpio_init(MIC_PIN);
@@ -22,17 +21,14 @@ void mic_init() {
 }
 
 float mic_read() {
-    static float smoothed_value = 0;
-    float raw_value = ADC_ADJUST(adc_read());
-    smoothed_value = (SMOOTHING_FACTOR * raw_value) + ((1 - SMOOTHING_FACTOR) * smoothed_value);
-    return smoothed_value;
+    return ADC_ADJUST(adc_read());
 }
 
 bool detect_clap() {
     int peak_count = 0;
     float last_value = mic_read();
     
-    for (int i = 0; i < SAMPLE_COUNT; i++) {
+    for (int i = 0; i < 60; i++) {  
         float current_value = mic_read();
 
         if (current_value - last_value > MIC_THRESHOLD) {  
@@ -40,8 +36,8 @@ bool detect_clap() {
         }
 
         last_value = current_value;
-        sleep_us(300); 
+        sleep_us(200);  
     }
 
-    return peak_count >= MIN_PEAKS;
+    return peak_count >= MIN_PEAKS;  
 }
