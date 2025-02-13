ba45c2b3214016fe09529e5834d40cfaf145912b
Herbert Gabriel <alencargabriel1010@gmail.com>
2025-02-13 17:19:06 -0300

    alteração dos valores de leitura do microfone

--- microfone.c
+++ microfone.c
@@ -7,10 +7,12 @@
 #define MIC_CHANNEL 2
 #define MIC_PIN (26 + MIC_CHANNEL)
 
-#define MIC_THRESHOLD 0.7f  
+#define MIC_THRESHOLD 0.6f  
 #define MIN_PEAKS 3 
 #define ADC_ADJUST(x) (x * 3.3f / (1 << 12u) - 1.65f)
 #define ADC_CLOCK_DIV 96.f
+#define SAMPLE_COUNT 50  
+#define SMOOTHING_FACTOR 0.2f 
 
 void mic_init() {
     adc_gpio_init(MIC_PIN);
@@ -20,14 +22,17 @@ void mic_init() {
 }
 
 float mic_read() {
-    return ADC_ADJUST(adc_read());
+    static float smoothed_value = 0;
+    float raw_value = ADC_ADJUST(adc_read());
+    smoothed_value = (SMOOTHING_FACTOR * raw_value) + ((1 - SMOOTHING_FACTOR) * smoothed_value);
+    return smoothed_value;
 }
 
 bool detect_clap() {
     int peak_count = 0;
     float last_value = mic_read();
     
-    for (int i = 0; i < 50; i++) {  
+    for (int i = 0; i < SAMPLE_COUNT; i++) {
         float current_value = mic_read();
 
         if (current_value - last_value > MIC_THRESHOLD) {  
@@ -35,8 +40,8 @@ bool detect_clap() {
         }
 
         last_value = current_value;
-        sleep_us(400);  
+        sleep_us(300); 
     }
 
-    return peak_count >= MIN_PEAKS;  
+    return peak_count >= MIN_PEAKS;
 }
