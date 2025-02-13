22128a46a2459c95ed21283c34dd8886447fc9c8
Herbert Gabriel <alencargabriel1010@gmail.com>
2025-02-13 20:49:05 -0300

    Update microfone.c

--- microfone.c
+++ microfone.c
@@ -7,7 +7,7 @@
 #define MIC_CHANNEL 2
 #define MIC_PIN (26 + MIC_CHANNEL)
 
-#define MIC_THRESHOLD 0.5f  
+#define MIC_THRESHOLD 0.7f  
 #define MIN_PEAKS 2
 #define ADC_ADJUST(x) (x * 3.3f / (1 << 12u) - 1.65f)
 #define ADC_CLOCK_DIV 96.f
